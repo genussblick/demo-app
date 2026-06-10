41d9ae6bbedf75e37779c4ad86dab64f081dfff9
yeabsra-leul <74534830+yeabsra-leul@users.noreply.github.com>
2026-06-10 18:45:54 +0300

    update app name

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Demo App',
+  title: '2603-layout-app',
   description: 'Created with Next.js',
   generator: 'Next.js',
   icons: {
